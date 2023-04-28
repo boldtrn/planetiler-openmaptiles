a6a3599bdefd4fff6dcc013cf6a0d756bd6aea81
boldtrn <boldtrn@gmail.com>
2023-04-28 16:27:11 +0200

    Update OMT

--- src/main/java/org/openmaptiles/layers/Transportation.java
+++ src/main/java/org/openmaptiles/layers/Transportation.java
@@ -172,7 +172,7 @@ public class Transportation implements
       entry(FieldValues.CLASS_SERVICE, 13),
       entry(FieldValues.CLASS_MINOR, 13),
       entry(FieldValues.CLASS_RACEWAY, 12),
-      entry(FieldValues.CLASS_TERTIARY, 11),
+      entry(FieldValues.CLASS_TERTIARY, 9),
       entry(FieldValues.CLASS_BUSWAY, 11),
       entry(FieldValues.CLASS_BUS_GUIDEWAY, 11),
       entry(FieldValues.CLASS_SECONDARY, 9),
